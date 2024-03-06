aaf3459c26c96ea585b3ac69f77aa55c346372f4
RJW20 <robertjwatts84@outlook.com>
2024-03-06 18:06:36 +0000

    Make to_pixels more central

--- pacman_app/drawer.py
+++ pacman_app/drawer.py
@@ -7,11 +7,11 @@ def to_pixels(coordinate: Position | tuple[int, int],
     """Convert a character position or tile on the map to pixel position on the screen."""
 
     if isinstance(coordinate, Position):
-        pixel_x = coordinate.true_x * tile_size + tile_size / 2
-        pixel_y = coordinate.true_y * tile_size + tile_size / 2
+        pixel_x = coordinate.true_x * tile_size + tile_size // 2
+        pixel_y = coordinate.true_y * tile_size + tile_size // 2
 
     elif isinstance(coordinate, tuple):
-        pixel_x = coordinate[0] * tile_size + tile_size / 2
-        pixel_y = coordinate[1] * tile_size + tile_size / 2
+        pixel_x = coordinate[0] * tile_size + tile_size // 2
+        pixel_y = coordinate[1] * tile_size + tile_size // 2
 
     return pixel_x, pixel_y
