c7815093fee1294c612d32bfb42a2b7f04377034
RJW20 <robertjwatts84@outlook.com>
2024-03-08 17:37:54 +0000

    Reduced the dots as appropriate

--- pacman_app/map/tile.py
+++ pacman_app/map/tile.py
@@ -5,11 +5,19 @@ class Tile(Enum):
     """Types of tile on the map."""
 
     WALL = '|'
-    PATH = '.'
+    DOT = '0'
+    POWER_DOT = '8'
     BLANK = '_'
     NODE = 'O'
     RESTRICTED_NODE = 'o'
+    NODE_NO_FOOD = '.'
 
     @property
     def is_node(self) -> bool:
-        return self.name == 'NODE' or self.name == 'RESTRICTED_NODE'
+        return self.name == 'NODE' or self.name == 'RESTRICTED_NODE' or \
+               self.name == 'NODE_NO_FOOD'
+    
+    @property
+    def is_dot(self) -> bool:
+        return self.name == 'DOT' or self.name == 'POWER_DOT' or \
+               self.name == 'NODE'
