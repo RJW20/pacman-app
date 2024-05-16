51ed8618bd3ef002751a2e8c70b57e039f1d89e8
RJW20 <robertjwatts84@outlook.com>
2024-05-16 16:20:51 +0100

    Fix bug when swapping between speeds

--- pacman_app/characters/character.py
+++ pacman_app/characters/character.py
@@ -8,6 +8,7 @@ class Character:
     def __init__(self) -> None:
         self.position: Position
         self.direction: Direction
+        self.step_size: int
 
     @property
     def speed(self) -> Speed:
@@ -28,24 +29,24 @@ class Character:
         """
 
         return self.position.offset_x == 0 and self.position.offset_y == 0 and \
-            int(self.speed.step_sizes[self._speed_count - 1]) != 0
+            self.step_size != 0
 
     def move(self) -> None:
         """Update position using direction and speed."""
 
-        step_size = int(self.speed.step_sizes[self._speed_count])
+        self.step_size = int(self.speed.step_sizes[self._speed_count])
         offset_x = self.position.offset_x
         offset_y = self.position.offset_y
         x_change = self.direction.value.d_x
         y_change = self.direction.value.d_y
 
         #ensure land on node centers
-        if step_size == 2:
+        if self.step_size == 2:
             if x_change == 1 and offset_x == -1 or \
                x_change == -1 and offset_x == 1 or \
                y_change == 1 and offset_y == -1 or \
                y_change == -1 and offset_y == 1:
-                step_size = 1
+                self.step_size = 1
 
-        self.position += self.direction.value * step_size
+        self.position += self.direction.value * self.step_size
         self._speed_count = (self._speed_count + 1) % 16
